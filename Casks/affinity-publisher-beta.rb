e539b5e0e954f9e74563b9ae3c1c98395c2561c1
jue144 <79714573+jue144@users.noreply.github.com>
2021-02-26 17:56:39 +0100

    Update affinity-publisher-beta.rb

--- Casks/affinity-publisher-beta.rb
+++ Casks/affinity-publisher-beta.rb
@@ -1,5 +1,5 @@
 cask "affinity-publisher-beta" do
-  version "1.8.4.663"
+  version "1.9.1.979"
   sha256 "c51e433a46054023e1b0c55b95b767e8c49a28b22af5ce5008ed33007d72f461"
 
   url "https://affinity-beta.s3.amazonaws.com/download/affinity-publisher-customer-beta-#{version}.zip",
